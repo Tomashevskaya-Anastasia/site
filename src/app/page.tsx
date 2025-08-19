a0eccda44890b637a88fe8305726ca99ecd9a2b9
Tomashevskaya Anastasia <128850738+Tomashevskaya-Anastasia@users.noreply.github.com>
2025-08-19 23:39:08 +0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HeaderExact from '@/components/HeaderExact';
+import HeroMogga from '@/components/HeroMogga';
+export default function Page() {
+  return (
+    <>
+      <HeaderExact />
+      <HeroMogga />
+    </>
+  );
+}
