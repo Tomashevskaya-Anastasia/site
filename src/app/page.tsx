522d1377012a17c36c3b27a2f08a004215a1d0e4
Tomashevskaya Anastasia <128850738+Tomashevskaya-Anastasia@users.noreply.github.com>
2025-08-22 15:02:52 +0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import HeaderExact from '@/components/HeaderExact';
+import HeroMogga from '@/components/HeroMogga';
+import MoggaCave from '@/components/MoggaCave';
+import RoadmapWall from '@/components/RoadmapWall';
+import TokenomicsSection from '@/components/TokenomicsSection';
+import Footer from '@/components/Footer';
+
+export default function Page() {
+  return (
+    <>
+      <HeaderExact />
+      <HeroMogga />
+      <MoggaCave />
+      <RoadmapWall />
+      <TokenomicsSection />
+      
+
+
+    </>
+  );
+}
